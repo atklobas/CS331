f090a6c18bf3918765f8900a6f1c8f7ad9997b80
Anthony Klobas <atklobas@gmail.com>
2017-11-17 09:27:51 -0800

    Changed name capitalization for consistancy

--- Views/index.php
+++ Views/index.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="<?php printURL()?>StudentInfo">Get info about specific student</a></li>
-    <li><a href="<?php printURL()?>StudentInfoLastname">Get info about specific student by Last Name</a></li>
+    <li><a href="<?php printURL()?>StudentInfoLastName">Get info about specific student by Last Name</a></li>
     <li><a href="<?php printURL()?>ListFckups">Get list of students having failed 2+ quarters</a></li>
     <li><a href="<?php printURL()?>ListAllStudents">List all students</a></li>
     <li><a href="#">Upload data</a></li>
