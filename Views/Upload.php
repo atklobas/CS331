cff3ea6adba7928720f096f034c3e384a8d2867f
nfsrun <kevin.tran@bellevuecollege.edu>
2017-11-26 12:45:33 -0800

    Upload.php's code is uploaded now...

---
+++ Views/Upload.php
@@ -0,0 +1,21 @@
+<?php if(is_null($_GET['file_loc'])){?>
+    <form action="StudentInfo.php", method="get">
+        <label for="file_loc">CSV File Location to add students: </label><input type="file" name="file_loc" id="file_loc" accept=".csv"></input>
+        <input type="submit" id="submit"></input>
+    </form>
+<?php }else{
+    //include_once('util.php');
+    include_once ("Controller.php");
+    include_once("MysqlLogin.php");
+    $file = fopen($_GET['file_loc'],"r");
+    $data = fgetcsv($file);
+    $i=0;
+    while(!is_null($data)) {
+        Upload_data($data);
+        $data = fgetcsv($file);
+    }
+    echo '<br/>';
+    if(!is_null($_GET['file_loc'])) {
+        fclose($file);
+    }
+?>
